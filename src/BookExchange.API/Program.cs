ebc2658d8b321a057cdea8c93f1e1fc238e016a7
tareqateiklab <tateik@hawk.iit.edu>
2025-07-11 19:16:31 +0400

    fix: move Swagger middleware outside of development check

--- src/BookExchange.API/Program.cs
+++ src/BookExchange.API/Program.cs
@@ -34,11 +34,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
