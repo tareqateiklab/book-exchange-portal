7468667e602c60e45f885f20c7ac34f480b67942
tareqateiklab <tateik@hawk.iit.edu>
2025-07-14 14:28:13 +0400

    feat: Implement home landing page layout and content with card components

--- src/BookExchange.Web/src/app/components/home/home.ts
+++ src/BookExchange.Web/src/app/components/home/home.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, RouterModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
